9e23628f6887d85bba5cfeb33d1d848b8593875b
Matt <mhooper72@gmail.com>
2018-04-06 13:32:45 -0600

    added context wrapper

--- src/App.js
+++ src/App.js
@@ -1,41 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+// NEW CONTEXT
+const AppContext = React.createContext();
+
+// PROVIDER COMPONENT
+class AppProvider extends Component {
+  state = {
+    species: 'narwhale',
+    name: 'Ned',
+    age: 4,
+    cool: true,
+    lengthofTusk: 10,
+    isSubmerged: true
+  };
+  render() {
+    return (
+      <AppContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () =>
+            this.setState(prevState => ({
+              age: prevState.age + 1
+            })),
+          levelUpTusk: () =>
+            this.setState(prevState => ({
+              lengthofTusk: prevState.lengthofTusk + 1
+            }))
+        }}
+      >
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
 
 const Ocean = props => (
-  <div className="family">
-    <Animal {...props} />
+  <div className="ocean">
+    <Animal />
   </div>
 );
 
 class Animal extends Component {
   render() {
-    const animals = this.props;
     return (
       <div>
-        <ul>
-          <p>HI! My name is {animals.name}</p>
-          <li>I am a {animals.species}</li>
-          <li>I am {animals.age} years old</li>
-        </ul>
+        <AppContext.Consumer>
+          {context => (
+            <Fragment>
+              <p>My name is {context.state.name}</p>
+              <p>I'm a {context.state.species}</p>
+              <p>I'm {context.state.age}</p>
+              <button onClick={context.growAYearOlder}>Happy Birthday {context.state.name}</button>
+              <p>Current Tusk Level: {context.state.lengthofTusk}</p>
+              <button onClick={context.levelUpTusk}>Level Up Tusk</button>
+            </Fragment>
+          )}
+        </AppContext.Consumer>
       </div>
     );
   }
 }
 
 class App extends Component {
-  state = {
-    species: 'narwhale',
-    name: 'Ned',
-    age: 80,
-    cool: true,
-    isSubmerged: true
-  };
-
   render() {
     return (
-      <div>
-        <p>I'm the app</p>
-        <Ocean {...this.state} />
-      </div>
+      <AppProvider>
+        <div>
+          <p>I'm the the Universe</p>
+          <Ocean />
+        </div>
+      </AppProvider>
     );
   }
 }
